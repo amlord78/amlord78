14443967aa798a4cf922ee0eb61ee23f57f01d34
amlord78 <88088117+amlord78@users.noreply.github.com>
2023-08-03 12:41:44 +0530

    Update 2.php

--- 2.php
+++ 2.php
@@ -1,3 +1,13 @@
 <?php
-     header('location:http://127.0.0.1');
+$file = 'test.pdf';
+$filename = 'test.pdf'; /* Note: Always use .pdf at the end. */
+
+header('Content-type: application/pdf');
+header('Content-Disposition: inline; filename="' . $filename . '"');
+header('Content-Transfer-Encoding: binary');
+header('Content-Length: ' . filesize($file));
+header('Accept-Ranges: bytes');
+
+@readfile($file);
+
 ?>
